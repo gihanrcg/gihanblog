84ac7026beb70a03069f34573a2531c71f9b2255
Gihan Siriwardhana <gihan.siriwardhana@ifsworld.com>
2020-04-03 23:27:11 +0530

    react js 1 added

---
+++ src/routes/posts/2020-04-03-IntroductionToReactJs_1/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Introduction to React JS - Part 01`,
+  tags: ['reactjs','web-development'],
+  spoiler: "Concepts and the usage of the react js framework",
+  getContent: () => import('./document.mdx'),
+}
