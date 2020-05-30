96e2d8e133dd74a4eca0f851c8ff6699ff15afb3
ifs-gisilk <gihan.siriwardhana@ifsworld.com>
2020-05-30 20:13:03 +0530

    updated

---
+++ src/routes/posts/2020-04-05-introductiontoreactjstwo/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Form Handling and Routing in React JS - Part 02`,
+  tags: ['reactjs','web-development', 'routing'],
+  spoiler: "Web development with react using bootstrap and form handling , route",
+  getContent: () => import('./document.mdx'),
+}
